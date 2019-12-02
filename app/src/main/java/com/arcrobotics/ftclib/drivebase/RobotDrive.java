e91eba2c0634523754eaa2d8d88ebba342f2cd8a
echo # <jisenberg1050@gmail.com>
2019-12-02 14:27:26 -0500

    Working on RobotDrive

--- app/src/main/java/com/arcrobotics/ftclib/drivebase/RobotDrive.java
+++ app/src/main/java/com/arcrobotics/ftclib/drivebase/RobotDrive.java
@@ -1,5 +1,21 @@
 package com.arcrobotics.ftclib.drivebase;
 
-public interface RobotDrive {
+public abstract class RobotDrive {
+
+    public enum MotorType {
+        kFrontLeft(0),
+        kFrontRight(1),
+        kBackLeft(2),
+        kBackRight(3),
+        kLeft(0),
+        kRight(1),
+        kBack(2);
+
+        public final int value;
+
+        MotorType(int value) {
+            this.value = value;
+        }
+    }
 
 }
